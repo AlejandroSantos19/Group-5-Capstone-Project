57b0b86aa4940e1e7bb8908e61fb3206a59b4df7
cjatalyn360-ops <cjatalyn360@gmail.com>
2026-03-01 02:15:52 -0500

    Add files via upload

--- Calendar.js
+++ Calendar.js
@@ -1,6 +1,18 @@
+/*
+ * calendar.js
+   Functions:
+ - Handles calendar and month navigation.
+ - Displays the current month and year
+ - Generates correct number of days for each month
+ - Aligns first day of month with correct weekday
+ - Allows navigation to previous and next months
+ */
+
+// Store the currently displayed date 
 let currentDate = new Date();
 
 function renderCalendar() {
+// Get references to HTML elements used for display
   const monthYear = document.getElementById("monthYear");
   const calendarDates = document.getElementById("calendarDates");
 
@@ -8,13 +20,18 @@ function renderCalendar() {
   const month = currentDate.getMonth();
 
   const firstDay = new Date(year, month, 1).getDay();
-  const lastDate = new Date(year, month + 1, 0).getDate();
 
+  // Determines the total number of days in the month
+  // (Using month + 1 and day 0 gives last day of current month)
+  const lastDate = new Date(year, month + 1, 0).getDate();
+  
+  // The array of month names
   const monthNames = [
     "January","February","March","April","May","June",
     "July","August","September","October","November","December"
   ];
 
+  //Display for the formatted month and year, e.g. "March 2026" title
   monthYear.innerText = `${monthNames[month]} ${year}`;
 
   calendarDates.innerHTML = "";
@@ -29,16 +46,16 @@ function renderCalendar() {
     calendarDates.innerHTML += `<div class="date">${day}</div>`;
   }
 }
-
+// Moves the calendar to the previous month then re renders the display
 function prevMonth() {
   currentDate.setMonth(currentDate.getMonth() - 1);
   renderCalendar();
 }
-
+// Moves the calendar to the next month . . .
 function nextMonth() {
   currentDate.setMonth(currentDate.getMonth() + 1);
   renderCalendar();
 }
 
-// Run when page loads
+// Run initial render when page first loads
 renderCalendar();
