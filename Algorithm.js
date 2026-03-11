5da93086bc890197e98b5db7050b320f5b1f2940
AlejandroSantos19 <alejandrosantos060@icloud.com>
2026-03-10 20:56:13 -0400

    Add files via upload

--- Algorithm.js
+++ Algorithm.js
@@ -1,57 +1,26 @@
 
-//This function checks to see if there are employee objects in the (JSON local storage) 
-//if it hasn't been created then the function creates it.
-function createDatabase(){
-
-  //The if statement checks and sees if there are no employee objects in local storage, if not, it creates an empty array of employees.
-    if (!localStorage.getItem("employees")) {
-
-      //The localStorage can only store strings so everything in the array is converted to a string so that it can be stored
-      localStorage.setItem("employees", JSON.stringify([]));
-    }
-  }
-  
-
-// This function adds a new employee to our database (JSON local storage)
-function addEmployee(FirstName, LastName, DateOfBirth, Position, HourlyWage, Email) {
-    // Get the current employees from localStorage
-    // JSON.parse converts the string back into a JavaScript array
-    const employees = JSON.parse(localStorage.getItem("employees"));
-  
-    // Creates a new employee object
-    const newEmployee = {
-
-      // Date.now() gives a unique number based on the time and can be used as an ID. An example would be (17342362342)
-      id: Date.now(),
-      FirstName: FirstName,
-      LastName: LastName,
-      DateOfBirth: DateOfBirth,
-      Position: Position,
-      HourlyWage: HourlyWage,
-      Email: Email
-    };
-  
-    // Add the new employee object to the array
-    employees.push(newEmployee);
-  
-    // Save the updated array back to localStorage
-    // JSON.stringify converts data into a string
-    localStorage.setItem("employees", JSON.stringify(employees));
-  }
-
 //This function is called when the "Add Employee" button is clicked and 
 //takes care of retrieving values from html page so that the employee can be added.
-function handleAddEmployee(){
+async function handleAddEmployee(){
     //Retrieve values from add employee html fields
     const FirstName = document.getElementById("FirstName").value;
     const LastName = document.getElementById("LastName").value;
     const DateOfBirth = document.getElementById("DateOfBirth").value;
     const Position = document.getElementById("Position").value;
-    const HourlyWage = document.getElementById("HourlyWage").value
+    const HourlyWage = document.getElementById("HourlyWage").value;
     const Email = document.getElementById("Email").value;
+    const PhoneNumber = document.getElementById("PhoneNumber").value;
 
-    //Call addEmployee fucntion with the values retrieved from above to add employee
-    addEmployee(FirstName, LastName, DateOfBirth, Position, HourlyWage, Email);
+    const response = await fetch("http://localhost:3000/save", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      //Add the input fields inside JSON.stringify({____})
+      body: JSON.stringify({FirstName, LastName, DateOfBirth, Position, HourlyWage, Email, PhoneNumber})
+    })
+
+     //This displays a pop-up message to the user and says whether the data was able to be saved or not in to the mysql data base
+     const result = await response.json();
+     alert(result.message);
 
     //Clears all Add Employee input fields
     const EmployeeInputFields = document.querySelectorAll(".add-employee-input");
@@ -63,64 +32,79 @@ function handleAddEmployee(){
     displayEmployees();
 }
 
+
 //This function runs when the remove employee button is clicked and removes the specified employee
-function handleRemoveEmployee(id) {
+async function handleRemoveEmployee(id) {
 
+  //This brings up a pop up message and asks the user if they want to delete the employee
   if(confirm("Are you sure you want to remove this employee?")){
-    //Calls getEmployees function which returns an array of all the employee objects
-    let employees = getEmployees();
+    try{
+      //The url contains the employee id of the employee id we want to remove.
+      //This allows our server and MySQL to know which employee we want to remove
+      const response = await fetch(`http://localhost:3000/employees/${id}`, {
+      method: "DELETE"
+      });
 
-    //Filters out the removed employee
-    employees = employees.filter(emp => emp.id !== id);
-    localStorage.setItem("employees", JSON.stringify(employees));
+      //This is our response back to the server and lets the user know if the employee was able to be removed
+      const result = await response.json();
+      alert(result.message);
+
+      //Calls the function displayEmployees and displays all employees in the employee table.
+      displayEmployees();
+    } catch (error){
+        console.error("There was an error deleting the employee:", error)
+    }
   }
   else{
+    //This brings up a pop up message letting the user know they canceled the action of deleting an employee
     alert("You have canceled the action of deleting the employee");
   }
 
-
-  //Calls the function displayEmployees and displays all employees in the employee table.
-  displayEmployees();
 }
-
-//This function returns an array of all the employee objects
-function getEmployees(){
-  return JSON.parse(localStorage.getItem("employees"));
-}
-
 
 //This function reads all employees from localstorage and displays them in a table
-function displayEmployees() {
-    const table = document.getElementById("EmployeeTable");
+async function displayEmployees() {
+  try{
+      const response = await fetch("http://localhost:3000/employees");
+      const employees = await response.json();
 
-    //Removes all exisitng rows from the EmployeeTable
-    table.innerHTML = "";
+      const table = document.getElementById("EmployeeTable");
+
+      //Removes all exisitng rows from the EmployeeTable
+      table.innerHTML = "";
   
-    //getEmployees returns an array of all the employee objects and the loop parses through each one of them
-    //creating a new row in the employee table for each one.
-    getEmployees().forEach(employee => {
-      //Creates a new table row
-      const row = document.createElement("tr");
-  
-      //Fills the newly created row with the following
-      row.innerHTML = `
-        <td>${employee.FirstName} ${employee.LastName}</td>
-        <td>${employee.Position}</td>
-        <td>${employee.HourlyWage}</td>
-        <td>${employee.Email}</td>
-        <td>
-          <button onclick="handleRemoveEmployee(${employee.id})">
-            Remove
-          </button>
-        </td>
-      `;
-      
-      //Adds the new row to the table
-      table.appendChild(row);
-    });
-  }
+      //This loops through all of the employees and displays their information onto our html table
+      employees.forEach(employee => {
+
+        console.log(employee);
+
+        //Creates a new table row
+        const row = document.createElement("tr");
+    
+        //Fills the newly created row with the following
+        //For each {employee. } the column name has to be the same as our MySQL employees table column names
+        row.innerHTML = `
+          <td>${employee.first_name} ${employee.last_name}</td>
+          <td>${employee.position}</td>
+          <td>${employee.hourly_wage}</td>
+          <td>${employee.email}</td>
+          <td>${employee.phone_number}</td>
+          <td>
+            <button onclick="handleRemoveEmployee(${employee.id})">
+              Remove
+            </button>
+          </td>
+        `;
+        
+        //Adds the new row to the table
+        table.appendChild(row);
+      });
+    } catch(error) {
+      console.error("There was an error loading the employees table: ",  error);
+    }
+
+}
 
 
-//These function run when the page loads
-createDatabase();
+//These function run when the page loads and displays all of our employees in our website table.
 displayEmployees();
---
+++ Server.js
@@ -0,0 +1,168 @@
+//Allows us to create a server (A Node.js Module)
+const http = require("http");
+
+//Allows us to communicate with our mysql database (A Node.js library)
+const mysql = require("mysql2");
+
+//This allows us to create a connection with our mysql database with the given information
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "IUSB2026!",
+    database: "Test_db"
+});
+
+//This actually connects us to our mysql database and informs us whether the connection was a success or if an error(err) is thrown
+connection.connect((err) => {
+
+    //Checks if there was an error connecting to the database
+    if(err){
+        //Displays this if there was an error connecting to the database in our console
+        console.log("There was an error connecting to the database :(");
+        return;
+    }
+
+    //Displays this if connection was successful
+    console.log("A connection has been established :)");
+})
+
+
+//This creates an HTTP server
+// req = is the request from the browser to our browser
+// res = is the response we send back to the browser from our server
+const server = http.createServer((req, res) => {
+
+    //This tells the web browser that our server allows any website to send requests (since we are only running our server locally, only our local website can send requests)
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    //This tell the web browser what kind of requests our server can recieve
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
+    //This allows the web browser to send a "Content-Type" header to our server such as ("Content-Type": "application/json")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    //Our server first checks to see if the request is of type "OPTIONS" (A preflight request before sending something like a GET, POST, or others)
+    //The "OPTIONS" req asks our server what kind of requests it accepts and lets our browser know what kind of requests our server can recieve.
+    if (req.method === "OPTIONS") {
+        
+        //This adds a 200 OK status code which indicates that the request has succeeded to our response
+        res.writeHead(200);
+
+        //This ends our response and is sent back to the browser
+        res.end();
+        return;
+    }
+
+
+    //Checks if the req is a "POST" method (A method used to send and update data)
+    //Checks if the req is to the "/save" endpoint which is used to identify what the request wants to do (Save data).
+    //This if statment runs if our webbrowser wants to save data to our MySQL database
+    if(req.method === "POST" && req.url === "/save"){
+
+        //This is used to store data sent from the browser
+        let sentData = "";
+
+        //Data on browsers is sent in chunks so we add all the chucnks together and store it in our variable "sentData"
+        req.on("data", chunk => {
+            sentData = sentData + chunk.toString();
+           //console.log(sentData);
+        })
+
+        //This runs once all of the sent data has been recieved and added to "sentData" variable
+        req.on("end", () => {
+
+        //********** This section can be changed to add more input fields  **********/
+            //This converts all the data that was sent into a javascript object
+            const data = JSON.parse(sentData);
+
+            //This looks and stores the name, email, and age value from our "data" variable above
+            const FirstName = data.FirstName;
+            const LastName = data.LastName;
+            const DateOfBirth = data.DateOfBirth;
+            const Position = data.Position;
+            const HourlyWage =parseFloat(data.HourlyWage);
+            const Email = data.Email;
+            const PhoneNumber = data.PhoneNumber;
+
+            //This is our sql statment that we will use to add the name into our MySQL database
+            //The ? are place holder values
+            const sql = "INSERT INTO employees (first_name, last_name, date_of_birth, position, hourly_wage, email, phone_number) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        //********** This section can be changed to add more input fields **********/
+
+
+            //This runs and querys the sql statment we created above
+                //The values we want to store must go in the [] 
+            connection.query(sql, [FirstName, LastName, DateOfBirth, Position, HourlyWage, Email, PhoneNumber], (err, result) => {
+
+                //This runs if there was an error adding the employee to our MySQL database
+                if(err){
+                    //This sends a 500 Internal Server Error code which indicates there was a problem with the server.
+                    res.writeHead(500, {"Content-Type": "application/json"});
+                    res.end(JSON.stringify({message: "Database error"}));
+                    return
+                }
+
+                //If everything worked okay then this runs and sends a message that the employee was saved to our MySQL database.
+                //This sends a 200 OK status code which indicates that the request has succeeded
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({message: "Employee information was saved!"}));
+            });
+        });
+    }
+    
+    //This if statement will run if our web browser wants to retrieve data
+    if(req.method === "GET" && req.url === "/employees"){
+        
+        //This is our sql statement that will be used to retrieve all the columns and rows from our employees table in MySQL
+        const sql = "SELECT * FROM employees";
+
+        //This runs our sql statement created above to retrieve all the employees
+        connection.query(sql, (err, results) => {
+            if(err){
+                //This sends a 500 Internal Server Error code which indicates there was a problem with the server.
+                res.writeHead(500, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({message: "Database error"}));
+                return;
+            }
+
+            //If everything worked okay then this runs and sends the results in form of JSON.
+            //This sends a 200 OK status code which indicates that the request has succeeded
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(results));
+        });
+    }
+
+    //This checks our webrowser sent a DELETE req and if was it for an employee
+    if(req.method === "DELETE" && req.url.startsWith("/employees/")){
+        console.log(req.url);
+
+        //Get the employee id from end of the url sent
+        //How it works: This gets the url, splits the strings at each /, creates an array of each string, and selects the array index 2 which is the employee id.
+        //The req url will look like  /employees/23 with 23 being the id of the employee
+        const id = req.url.split("/")[2];
+        console.log("Employee with this ID was removed: " + id);
+
+        //This is the sql statment that will be used to delete the employee
+        const sql = "DELETE FROM employees WHERE id = ?"
+
+        //This runs the sql statement that we created above to delete the employee with the specified id
+        connection.query(sql, [id], (err, results) => {
+
+            //This checks if there was en error deleting the employee
+            if(err){
+                //This sends a 500 Internal Server Error code which indicates there was a problem with the server.
+                res.writeHead(500, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({message: "Database error"}));
+                return;
+            }
+
+            //If everything worked okay then this runs and sends a message to indicate the the employee was removed
+            //This sends a 200 OK status code which indicates that the request has succeeded
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({message: "Employee was deleted successfully"}));
+        });
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
